32681713f1f0f148677527fd780ceca9f823ceef
Samuel Correia <thenenouno1@gmail.com>
2025-07-30 09:15:22 -0300

    30/07/2025 - finalizacao clean code

--- src/main/java/com/senai/aula_clean_code/depois_sistema_padronizado_check_in/CheckInFlySecure.java
+++ src/main/java/com/senai/aula_clean_code/depois_sistema_padronizado_check_in/CheckInFlySecure.java
@@ -5,6 +5,7 @@ public interface CheckInFlySecure {
     double PESO_MAX_BAGAGEM_INTERNACIONAL = 32;
     double QTD_MAXIMA_BAGAGENS = 5;
 
+
     void validarDocumento(String cpf);
     void emitirCartaoEmbarque(String nome);
     void realizarCheckIn(Passageiro passageiro);
@@ -17,22 +18,26 @@ public interface CheckInFlySecure {
         System.out.println("Check-in seguro realizado pela FlySecure");
     }
 
-    default void validarTipoViagem(String tipoViagem) {
+    default void validarPesoBagagem(String tipoViagem, int pesoBagagem) {
         if (tipoViagem.equals("Nacional")) {
-
+            validarPesoBagagemNacional(pesoBagagem);
+        } else if (tipoViagem.equals("Internacional")) {
+            validarPesoBagagemInternacional(pesoBagagem);
+        } else {
+            System.out.println("Tipo de viagem inválido! Check-in cancelado.");
         }
     }
 
-    default void validarPesoBagagemNacional(int pesoBagagemNacional){
-        if (pesoBagagemNacional > PESO_MAX_BAGAGEM_NACIONAL) {
+    default void validarPesoBagagemNacional(int pesoBagagem){
+        if (pesoBagagem > PESO_MAX_BAGAGEM_NACIONAL) {
             System.out.println("Bagagem não autorizada (acima do peso máximo para viagens nacionais)!");
         } else {
             System.out.println("Bagagem autorizada!");
         }
     }
 
-    default void validarPesoBagagemInternacional(int pesoBagagemInternacional){
-        if (pesoBagagemInternacional > PESO_MAX_BAGAGEM_INTERNACIONAL) {
+    default void validarPesoBagagemInternacional(int pesoBagagem){
+        if (pesoBagagem > PESO_MAX_BAGAGEM_INTERNACIONAL) {
             System.out.println("Bagagem não autorizada (acima do peso máximo para viagens internacionais)!");
         } else {
             System.out.println("Bagagem autorizada!");
--- src/main/java/com/senai/aula_clean_code/depois_sistema_padronizado_check_in/GOL.java
+++ src/main/java/com/senai/aula_clean_code/depois_sistema_padronizado_check_in/GOL.java
@@ -1,10 +1,11 @@
 package com.senai.aula_clean_code.depois_sistema_padronizado_check_in;
 
 public class GOL implements CheckInFlySecure {
+    private static final int QTD_CARACTERES_CPF = 14;
 
     @Override
     public void validarDocumento(String cpf) {
-        if (cpf != null && cpf.length() == 14) {
+        if (cpf != null && cpf.length() == QTD_CARACTERES_CPF) {
             System.out.println("Documento válido.");
         } else {
             System.out.println("Documento inválido.");
@@ -22,14 +23,8 @@ public class GOL implements CheckInFlySecure {
 
         validarDocumento(passageiro.getCpf());
         mensagemDadosValidados(passageiro.getNome());
-        if (passageiro.getTipoViagem() == 1) {
-            validarPesoBagagemNacional(passageiro.getPesoBagagem());
-            emitirCartaoEmbarque(passageiro.getNome());
-            CheckInFlySecure.mensagemCheckIn();
-        } else if (passageiro.getTipoViagem() == 2) {
-            validarPesoBagagemInternacional(passageiro.getPesoBagagem());
-        } else {
-            System.out.println("Tipo de viagem inválido! Check-in cancelado.");
-        }
+        validarPesoBagagem(passageiro.getTipoViagem(), passageiro.getPesoBagagem());
+        emitirCartaoEmbarque(passageiro.getNome());
+        CheckInFlySecure.mensagemCheckIn();
     }
 }
